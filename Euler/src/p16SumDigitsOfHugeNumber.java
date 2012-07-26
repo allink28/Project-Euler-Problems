371b29edf9e7c0627ed48f52f330bddf1571d8ba
Allen <Allink28@gmail.com>
2012-07-26 18:00:43 -0400

    Problem 16 complete (Using .pow it takes 1.5 milliseconds. Using my own exponentiation algorithm it takes 3 milliseconds.)

---
+++ Euler/src/p16SumDigitsOfHugeNumber.java
@@ -0,0 +1,42 @@
+import java.math.BigInteger;
+
+/**
+ * 215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+ * 
+ * What is the sum of the digits of the number 21000?
+ * 
+ * @author Allen Preville
+ * 
+ */
+public class p16SumDigitsOfHugeNumber {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		BigInteger number = new BigInteger("2");
+
+		//----------Step 1: Calculate--------------
+		//----Easy way to calculate----
+		//number = number.pow(1000);
+		
+		//--- Do it myself way: -------
+		for(int i=0; i<9; ++i){ //This is an efficient way of getting 2^512
+			number = number.multiply(number);
+		}	
+		BigInteger number2 = new BigInteger("2");
+		for(int i=0; i<488; ++i){
+			number = number.multiply(number2);
+		}
+		
+		//----------Step 2: Calculate--------------
+		String bigNumber = number.toString();
+		int length = bigNumber.length();
+		int sum = 0;
+		for(int i=0; i<length; ++i){
+			sum = Integer.parseInt(bigNumber.charAt(i)+"") + sum;
+		}
+		
+		System.out.println(sum);
+	}
+}
